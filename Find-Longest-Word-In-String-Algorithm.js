a04a78438bbab570f761b7870ab2b52b3ec325c9
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2022-07-17 13:57:01 +0000

    Added note on using SPLIT method instead of REGEX

--- Find-Longest-Word-In-String-Algorithm.js
+++ Find-Longest-Word-In-String-Algorithm.js
@@ -2,6 +2,9 @@ function findLongestWordLength(str) {
     //Break string into an array of words
     let wordRegex = /\w+/g
     let words = str.match(wordRegex);
+    //The above was achieved using Regex.
+    //However it could have been achieved using the string-method SPLIT().
+    //let words = str.split(" "); would return an array of words, using the delimiter of the space in the method.
     
     //Loop through the array to log the longest word or/and its length.
     let longestWord = [];
