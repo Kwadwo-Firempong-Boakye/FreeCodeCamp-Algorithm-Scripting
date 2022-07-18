65d8c2fc0290e7a3ea5ae86c1fd50d4795657355
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2022-07-18 15:46:04 +0000

    Add solution to truncate string using splice()

--- Truncate-String-To-Specific-Length.js
+++ Truncate-String-To-Specific-Length.js
@@ -7,3 +7,26 @@ function truncateString(str, num) {
   truncateString("A-tisket a-tasket A green and yellow basket", 8);
 
 
+  //Using splice method
+
+  function truncateString2(str, num) {
+    //convert str to an array because splice method only works on arrays
+    let strArr = [...str];
+    //use splice on new array to truncate array and store in new spliced array variable
+    let splicedStrArr = strArr.splice(0, num);
+    //loop through spliced array to re-assemble to new string or use toString() method if the string is longer than the set limit num
+    let splicedStr = ""
+    if(str.length > num){
+        for(let i = 0; i < splicedStrArr.length; i++){
+        splicedStr += splicedStrArr[i];
+        }
+    } else {
+        return str;
+    }
+    //create final string by adding "..." to splicedStr
+    let finalStr = splicedStr + "...";
+    return finalStr;
+  }
+
+  truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
