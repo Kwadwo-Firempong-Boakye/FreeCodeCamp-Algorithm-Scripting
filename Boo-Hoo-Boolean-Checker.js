21aff28f453aed891354637c251339d34cca8e66
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2022-07-18 16:36:58 +0000

    Check for data-type "boolean" in function argument

---
+++ Boo-Hoo-Boolean-Checker.js
@@ -0,0 +1,12 @@
+function booWho(bool) {
+
+//Use typeof method to ascertain if argument is boolean
+
+    if(typeof bool === "boolean"){
+      return true;
+    } else{
+      return false;
+    }
+  }
+  
+  booWho(null);
