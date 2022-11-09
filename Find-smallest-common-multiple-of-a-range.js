43fca7ec3e29690f5de6f6c4d4b642f70657cfb0
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2022-11-09 15:12:03 +0000

    Use Prime Factorization to derive LCM/SCM

--- Find-smallest-common-multiple-of-a-range.js
+++ Find-smallest-common-multiple-of-a-range.js
@@ -71,3 +71,102 @@ function smallestCommons2(arr) {
 }
 
 smallestCommons([2, 10]);
+
+//I DONT EVEN KNOW WHY I PUT MYSELF THROUGH THIS METHOD OF PRIME FACTORIZATION
+
+function smallestCommons(arr) {
+	let maxNum = Math.max(...arr);
+	let minNum = Math.min(...arr);
+	let fullArr = [];
+	for (let i = minNum; i <= maxNum; i++) {
+		if (i != 1) fullArr.push(i);
+	}
+
+	const determinePrimes = (num, arr1, arr2) => {
+		let dividers = [];
+		for (let i = 2; i < num; i++) {
+			dividers.push(i);
+		}
+		if (dividers.every((divider) => num % divider != 0)) {
+			arr1.push(num);
+			return "yes";
+		} else {
+			arr2.push(num);
+			return "no";
+		}
+	};
+
+	let primes = [];
+	let nonPrimes = [];
+
+	fullArr.forEach((fullNum) => {
+		determinePrimes(fullNum, primes, nonPrimes);
+	});
+
+	let tempPrimeFactors = [];
+	let primeFactors = [];
+	let uniquePrimeFactors = [];
+
+	nonPrimes.forEach((num) => {
+		let tempPrime = [];
+		let tempNonPrime = [];
+		for (let i = 2; i < num; i++) {
+			let quot = num;
+			if (determinePrimes(i, tempPrime, tempNonPrime) == "yes") {
+				while (quot % i == 0) {
+					tempPrimeFactors.push(i);
+					if (!uniquePrimeFactors.includes(i)) {
+						uniquePrimeFactors.push(i);
+					}
+					quot = quot / i;
+				}
+			}
+		}
+		primeFactors.push(tempPrimeFactors);
+		tempPrimeFactors = [];
+	});
+
+	let finalFactors = [];
+	let groupedFactors = [];
+	let nonRepeatedPrimes = [];
+
+	uniquePrimeFactors.forEach((num) => {
+		let tempGroup = [];
+		let length = 1;
+		let index = 0;
+		primeFactors.forEach((arr) => {
+			let temp = arr.filter((item) => item == num);
+			if (temp.length > 0) tempGroup.push(temp);
+		});
+		tempGroup.forEach((arr, i) => {
+			if (arr.length > length) {
+				length = arr.length;
+				index = i;
+			}
+		});
+		groupedFactors.push(tempGroup[index]);
+		finalFactors.push(tempGroup[index].reduce((acc, cur) => acc * cur));
+	});
+
+	let count = 0;
+	primes.forEach((prime) => {
+		groupedFactors.forEach((group) => {
+			if (group.includes(prime)) {
+				count += 1;
+			}
+		});
+		if (count == 0) {
+			finalFactors.push(prime);
+		}
+		count = 0;
+	});
+
+	let scm = finalFactors.reduce((acc, cur) => acc * cur);
+
+	return scm;
+}
+
+smallestCommons([1, 5]);
+smallestCommons([1, 13]);
+smallestCommons([2, 10]);
+smallestCommons([23, 18]);
